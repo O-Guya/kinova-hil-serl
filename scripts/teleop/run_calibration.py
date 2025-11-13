3f9156ff33dc27b939f48f0b518136179feb8f25
O-Guya <trahjy@163.com>
2025-11-13 10:44:51 +0800

    add test scripts.

--- scripts/teleop/run_calibration.py
+++ scripts/teleop/run_calibration.py
@@ -8,7 +8,7 @@ import argparse
 from pathlib import Path
 
 # 添加路径
-sys.path.insert(0, str(Path(__file__).parent.parent))
+sys.path.insert(0, str(Path(__file__).parent.parent.parent))
 
 
 from vision_pro_control.core.visionpro_bridge import VisionProBridge
